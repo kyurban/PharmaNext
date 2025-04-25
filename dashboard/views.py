0ee801cf9c0e2c81c9b30ed9eeab40ada18ba0a1
kurban15 <kyleurban99@gmail.com>
2025-04-25 10:15:19 -0700

    Removed QV access for technicians completely

--- dashboard/views.py
+++ dashboard/views.py
@@ -4,9 +4,12 @@ from patient_management.models import Patient
 from rx_pending.models import Prescription
 
 def dashboard(request):
+    is_pharmacist = request.user.groups.filter(name="Pharmacists").exists()
+
     context = {
         'count_qt': Prescription.objects.filter(status='Pending').count(),
         'count_qp': Prescription.objects.filter(status='In Production').count(),
         'count_qv': Prescription.objects.filter(status='Awaiting Verification').count(),
+        'is_pharmacist': is_pharmacist,
     }
     return render(request, 'dashboard.html', context)
--- login/views.py
+++ login/views.py
@@ -9,7 +9,7 @@ def login_user(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            return redirect('home')
+            return redirect('dashboard')
         else:
             messages.success(request, 'Invalid credentials')
             return render(request, 'login.html')
